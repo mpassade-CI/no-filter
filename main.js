9c67294509ea0896c7c82e82b883e59194d17eff
Michael Passade <michael.passade@codeimmersives.com>
2020-02-27 13:13:53 -0500

    done

--- main.js
+++ main.js
@@ -1,9 +1,48 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+function onlyOdds(arr){
+  const x = []
+  for (let i=0; i<arr.length; i++){
+    if (arr[i]%2!==0){
+      x.push(arr[i])
+    }
+  }
+  return x
+}
 
+function onlyEvens(arr){
+  const x = []
+  for (let i=0; i<arr.length; i++){
+    if (arr[i]%2===0){
+      x.push(arr[i])
+    }
+  }
+  return x
+}
 
+function shortNamesOnly(arr){
+  const x = []
+  for (let i=0; i<arr.length; i++){
+    if (arr[i].length<7){
+      x.push(arr[i])
+    }
+  }
+  return x
+}
 
+function dNames(arr){
+  const x = []
+  let y = ''
+  let z = 'Dd'
+  for (let i=0; i<arr.length; i++){
+    y = arr[i]
+    if (z.includes(y[0])){
+      x.push(y)
+    }
+  }
+  return x
+}
 
 
 
